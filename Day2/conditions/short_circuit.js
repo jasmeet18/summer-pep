1d925e42726b661c1079cf84b6248c08500bfe14
Jasmeet kour <114793934+jasmeet18@users.noreply.github.com>
2025-06-21 14:09:43 +0530

    Delete Day2/conditions directory

--- Day2/conditions/short_circuit.js
+++
@@ -1,8 +0,0 @@
-let isLoggedIn = false;
-let defaultName = "Guest";
-
-let displayName = isLoggedIn && "User"; // false
-console.log(displayName); // false
-
-let name = isLoggedIn || defaultName; // fallback
-console.log(name); // Guest
